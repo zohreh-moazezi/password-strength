4bd5cae24d8ac1e7f9d2f08fa41a243e6abf33f7
zohreh moazezi <zohre.m548@gmail.com>
2023-01-19 16:38:51 +0330

    password

---
+++ script.js
@@ -0,0 +1,27 @@
+// Difficulty: 4
+
+// RegEx: Regular Expression
+
+// Password: Invalid/Valid
+
+function checkPassword(password) {
+    let oneLowerCaseLetter = /(?=.+[a-z])/;
+    let oneUpperCaseLetter = /(?=.+[A-Z])/;
+    let oneDigit = /(?=.+[0-9])/;
+    let oneSpecialCharacter = /(?=.+[!@#$%^&*])/;
+    let minimumEightCharacters = /(?=.{8,})/;
+  
+    let isValid =
+      oneLowerCaseLetter.test(password) &&
+      oneUpperCaseLetter.test(password) &&
+      oneDigit.test(password) &&
+      oneSpecialCharacter.test(password) &&
+      minimumEightCharacters.test(password);
+  
+    return isValid
+      ? "Your password is valid"
+      : "Your password is invalid, try again";
+  }
+  
+  console.log(checkPassword("abc"));
+  console.log(checkPassword("9Ab!4567"));
